486ed2638befc3e97459d7ee9f8bfc929e5b4090
Roisteeve <167654420+Roisteeve@users.noreply.github.com>
2024-10-06 22:39:18 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "24105114159"
-global.ownername = "𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸"
+global.ownernomer = "50937042801"
+global.ownername = "Anonymous"
 global.ytname = "kenzo3146"
 global.socialm = "GitHub: toge012345"
 global.location = "Africa, Gabon, Libreville"
 
-global.ownernumber = '24105114159'  //creator number
-global.ownername = '𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸' //owner name
+global.ownernumber = '50937032801'  //creator number
+global.ownername = 'Anonymous' //owner name
 global.botname = '𝚃𝙾𝙶𝙴-𝙱𝚄𝙶-𝚅𝟸' //name of the bot
 
 //sticker details
@@ -21,7 +21,7 @@ global.themeemoji = '👾'
 global.wm = "𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaiuD4s4IBhI0fzbv40Z'
+global.link = 'https://google.com'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
